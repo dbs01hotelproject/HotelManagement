f2cde004b03fe4f3b6b678f74ba5839160e32c8c
MuyianKing <muyianKing@163.com>
2019-05-25 13:34:01 +0800

    2019/5/25

--- src/main/java/com/dbs/po/Customer.java
+++ src/main/java/com/dbs/po/Customer.java
@@ -1,47 +1,65 @@
 package com.dbs.po;
 
+/**
+ * 客户
+ * @author muyian
+ *
+ */
 public class Customer {
 
-	private int id;
-	private String username;
-	private String phone;
-	private String job;
+	private int c_customernumber;
+	private String c_name;
+	private String c_identity;
+	private String c_sex;
+	private String c_address;
+	private String c_tel;
 
-	public int getId() {
-		return id;
+	public int getC_customernumber() {
+		return c_customernumber;
 	}
 
-	public void setId(int id) {
-		this.id = id;
+	public void setC_customernumber(int c_customernumber) {
+		this.c_customernumber = c_customernumber;
 	}
 
-	public String getUsername() {
-		return username;
+	public String getC_name() {
+		return c_name;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
+	public void setC_name(String c_name) {
+		this.c_name = c_name;
 	}
 
-	public String getPhone() {
-		return phone;
+	public String getC_identity() {
+		return c_identity;
 	}
 
-	public void setPhone(String phone) {
-		this.phone = phone;
+	public void setC_identity(String c_identity) {
+		this.c_identity = c_identity;
 	}
 
-	public String getJob() {
-		return job;
+	public String getC_sex() {
+		return c_sex;
 	}
 
-	public void setJob(String job) {
-		this.job = job;
+	public void setC_sex(String c_sex) {
+		this.c_sex = c_sex;
 	}
 
-	@Override
-	public String toString() {
-		return "Customer [id=" + id + ", username=" + username + ", phone=" + phone + ", job=" + job + "]";
+	public String getC_address() {
+		return c_address;
+	}
+
+	public void setC_address(String c_address) {
+		this.c_address = c_address;
+	}
+
+	public String getC_tel() {
+		return c_tel;
+	}
+
+	public void setC_tel(String c_tel) {
+		this.c_tel = c_tel;
 	}
 
 }
