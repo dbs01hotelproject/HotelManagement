6e5366448b8930b2f1463238e2f66f95609865f3
MuyianKing <muyianKing@163.com>
2019-05-26 14:27:34 +0800

    2019/5/26 杨峰

---
+++ src/main/java/com/dbs/po/RoomInformation.java
@@ -0,0 +1,57 @@
+ package com.dbs.po;
+
+/**
+ * 房间信息表
+ * 
+ * @author muyian
+ *
+ */
+public class RoomInformation {
+
+	private int r_number;
+	private String r_direction;
+	private int r_tpye;
+	private int r_equipment;
+	private int r_state;
+
+	public int getR_number() {
+		return r_number;
+	}
+
+	public void setR_number(int r_number) {
+		this.r_number = r_number;
+	}
+
+	public String getR_direction() {
+		return r_direction;
+	}
+
+	public void setR_direction(String r_direction) {
+		this.r_direction = r_direction;
+	}
+
+	public int getR_tpye() {
+		return r_tpye;
+	}
+
+	public void setR_tpye(int r_tpye) {
+		this.r_tpye = r_tpye;
+	}
+
+	public int getR_equipment() {
+		return r_equipment;
+	}
+
+	public void setR_equipment(int r_equipment) {
+		this.r_equipment = r_equipment;
+	}
+
+	public int getR_state() {
+		return r_state;
+	}
+
+	public void setR_state(int r_state) {
+		this.r_state = r_state;
+	}
+
+}
